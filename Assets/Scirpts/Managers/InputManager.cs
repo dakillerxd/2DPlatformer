393a8937d65f7287eb7c21b06f6380532a33603a
Daniel-Laptop <danielnoam1999@gmail.com>
2024-11-14 12:36:31 +0200

    Update
    worked on sfx

--- Assets/Scirpts/Managers/InputManager.cs
+++ Assets/Scirpts/Managers/InputManager.cs
@@ -13,12 +13,10 @@ public class InputManager : MonoBehaviour
     public static bool JumpWasPressed;
     public static bool JumpIsHeld;
     public static bool JumpWasReleased;
-    public static bool RunIsHeld;
     public static bool DashWasPressed;
     public static bool RestartWasPressed;
     private InputAction moveAction;
     private InputAction jumpAction;
-    private InputAction runAction;
     private InputAction dashAction;
     private InputAction restartAction;
     
@@ -54,7 +52,6 @@ public class InputManager : MonoBehaviour
 
         moveAction = playerInput.actions["Move"];
         jumpAction = playerInput.actions["Jump"];
-        runAction = playerInput.actions["Run"];
         dashAction = playerInput.actions["Dash"];
         togglePauseAction = playerInput.actions["TogglePause"];
         restartAction = playerInput.actions["Restart"];
@@ -68,24 +65,8 @@ public class InputManager : MonoBehaviour
         JumpWasPressed = jumpAction.WasPressedThisFrame();
         JumpIsHeld = jumpAction.IsPressed();
         JumpWasReleased = jumpAction.WasReleasedThisFrame();
-        RunIsHeld = runAction.IsPressed();
         DashWasPressed = dashAction.WasPressedThisFrame();
         TogglePauseWasPressed = togglePauseAction.WasPressedThisFrame();
         RestartWasPressed = restartAction.WasPressedThisFrame();
     }
-
-
-    private void SetAction(InputAction action, string name) {
-        
-        if (playerInput == null) {
-            Debug.LogError("PlayerInput component not found on InputManager GameObject!");
-            return; 
-        }
-        if (playerInput.actions[name] == null) {
-            Debug.LogError("InputAction " + name + " not found on PlayerInput component!");
-            return; 
-        }
-        action = playerInput.actions[name];
-    }
-    
 }

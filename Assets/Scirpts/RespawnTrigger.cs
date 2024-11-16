7c02c07c93beb11da40cc4ca77d8a1d811ba6e13
Daniel-PC <danielnoam1999@gmail.com>
2024-11-16 18:01:04 +0200

    Update
    work on level 4
    fixed respawn trigger

--- Assets/Scirpts/RespawnTrigger.cs
+++ Assets/Scirpts/RespawnTrigger.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class RespawnTrigger : MonoBehaviour
 {
     
-   private void OnTriggerEnter(Collider other)
+   private void OnTriggerEnter2D(Collider2D other)
    {
         if(!other.CompareTag("Player")) return;
         SoundManager.Instance?.PlaySoundFX("Player Fall off Map");

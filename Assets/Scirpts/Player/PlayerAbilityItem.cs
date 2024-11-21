3064ee8ee53479522f12e7b1e1cca65800a42770
Daniel-Laptop <danielnoam1999@gmail.com>
2024-11-21 18:29:06 +0200

    Update
    work on level 5

--- Assets/Scirpts/Player/PlayerAbilityItem.cs
+++ Assets/Scirpts/Player/PlayerAbilityItem.cs
@@ -2,12 +2,14 @@ using System;
 using System.Diagnostics;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerAbilityItem : MonoBehaviour
 {
 
     [Header("Settings")]
     [SerializeField] private PlayerAbilities ability;
+    [SerializeField] private UnityEvent[] eventsAfterTrigger;
     private bool _triggered;
     
     
@@ -28,6 +30,10 @@ public class PlayerAbilityItem : MonoBehaviour
             _triggered = true;
             PlayerController.Instance.ReceiveAbility(ability);
             CameraController.Instance.ShakeCamera(3f, 5f,2,2);
+            foreach (var e in eventsAfterTrigger)
+            {
+                e.Invoke();
+            }
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scirpts/TeleportTrigger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
+public class TeleportTrigger : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform teleportLocation;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    private bool _triggered;
+    
+    
+    private void Start()
+    {
+        // spriteRenderer.enabled = false;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(!other.CompareTag("Player") ||  !teleportLocation || _triggered) return;
+        
+        _triggered = true;
+
+        Vector3 XYPosition = new Vector3(teleportLocation.position.x, teleportLocation.position.y + 10f, CameraController.Instance.transform.position.z);
+        PlayerController.Instance.transform.position = teleportLocation.position;
+        // CameraController.Instance.transform.position = XYPosition;
+        
+        StartCoroutine(ResetTrigger());
+    }
+
+
+    private IEnumerator ResetTrigger()
+    {
+        yield return new WaitForSeconds(1f);
+        _triggered = false;
+    }
+   
+}
